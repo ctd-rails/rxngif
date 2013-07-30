da4ef1f8db47779cade916ef733524a32676671f
Arjun Venkataswamy <avenkat2@gmail.com>
2013-07-30 09:45:35 -0500

    adds a single RCAV for the pictures index

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,5 @@
+class PicturesController < ApplicationController
+  def index
+    render 'index'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rxngif::Application.routes.draw do
   # create an RCAV that supports the following request:
   # a user can type in '/pictures' into the address bar
   # and see a list of pictures
+  get "/pictures", { :controller => "pictures", :action => "index" }
 end

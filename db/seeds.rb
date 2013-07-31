1a583e65c59ae74fb009982e893bbef9947419ad
Arjun Venkataswamy <avenkat2@gmail.com>
2013-07-31 09:15:08 -0500

    adds seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Picture.destroy_all
+
+p1 = Picture.new
+p1.url = "http://i0.kym-cdn.com/photos/images/newsfeed/000/044/778/hatersgonnacat.jpg?1318992465"
+p1.description = "Cat"
+p1.favorite = false
+p1.save
+
+p2 = Picture.new
+p2.url = 'http://i0.kym-cdn.com/photos/images/newsfeed/000/039/080/5008_9c00_420.gif?1318992465'
+p2.description = "little kid"
+p2.favorite = false
+p2.save
+
+p3 = Picture.new
+p3.url = "http://i0.kym-cdn.com/photos/images/newsfeed/000/039/078/1257.jpg?1265669527"
+p3.description = "eagle"
+p3.favorite = true
+p3.save
+
+
+puts "3 pictures were created"
